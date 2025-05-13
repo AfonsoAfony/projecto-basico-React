f4883f9ab843bb6f539bad8f9d15615d8d8c6da7
Afonso <Afonydasilva@gmail.com>
2025-05-13 12:35:42 +0100

    Alterei o nome do arquivo detailsDesk.jsx para DetailsDesk.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import DetailsTask from './pages/detailstask.jsx';
+import DetailsTask from './pages/DetailsTask.jsx';
 
 const router = createBrowserRouter([
   {
